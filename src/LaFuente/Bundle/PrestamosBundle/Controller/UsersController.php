f0e1c01353c13db3d311f610d4198de04447ce66
Matias Pierobon <matias.pierobonm@gmail.com>
2015-08-12 13:58:57 -0300

    Se siguio con el maquetado

---
+++ src/LaFuente/Bundle/PrestamosBundle/Controller/UsersController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace LaFuente\Bundle\PrestamosBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UsersController extends Controller
+{
+    public function indexAction()
+    {
+        $users = $this->getDoctrine()->getRepository('UserBundle:User')->findAll();
+        return $this->render('PrestamosBundle:Users:index.html.twig', array('users' => $users));
+    }
+
+    public function newAction()
+    {
+        return $this->render('PrestamosBundle:Users:index.html.twig');
+    }
+
+    public function createAction()
+    {
+        return $this->render('PrestamosBundle:Users:index.html.twig');
+    }
+
+    public function showAction()
+    {
+        return $this->render('PrestamosBundle:Users:index.html.twig');
+    }
+
+    public function upateAction()
+    {
+        return $this->render('PrestamosBundle:Users:index.html.twig');
+    }
+
+    public function switchAction()
+    {
+        return $this->render('PrestamosBundle:Users:index.html.twig');
+    }
+
+    public function photoAction()
+    {
+        return $this->render('PrestamosBundle:Users:index.html.twig');
+    }
+}
